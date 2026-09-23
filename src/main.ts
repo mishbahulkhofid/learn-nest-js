b9ededca5277dfaadf5bd8b174ca17eb93697150
mishbahulkhofid <mishbahulkhofid0@gmail.com>
2026-09-23 11:25:47 +0700

    resolve merge conflict dari upstream/main

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,8 @@
 import { NestFactory } from '@nestjs/core';
-<<<<<<< HEAD
-import { AppModule, ObserveInstrument } from './app.module.js';
-
-async function bootstrap() {
-  const app = await NestFactory.create(AppModule, {
-    instrument: ObserveInstrument,
-  });
-=======
 import { AppModule } from './app.module.js';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
->>>>>>> upstream/main
   await app.listen(process.env.PORT ?? 3000);
 }
 await bootstrap();
